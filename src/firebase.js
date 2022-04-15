6e3c306ca358d63e0a3c51dbb042ecd0c2d67f36
Ronen Fromchuk <90146636+ronenfromchuk@users.noreply.github.com>
2022-04-15 22:11:49 +0200

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,13 @@ import "firebase/compat/auth";
 import "firebase/compat/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBsAkrfQVos5Hf6VuARVgS8a-SpjgoZ6g8",
-    authDomain: "twitter-clone-a4461.firebaseapp.com",
-    projectId: "twitter-clone-a4461",
-    storageBucket: "twitter-clone-a4461.appspot.com",
-    messagingSenderId: "96535948440",
-    appId: "1:96535948440:web:39dbec5bc4a56e441bb4ed",
-    measurementId: "G-GFSNQY0DGW"
+    apiKey: "**************************************************************************************",
+    authDomain: "**************************************************************************************",
+    projectId: "**************************************************************************************",
+    storageBucket: "**************************************************************************************",
+    messagingSenderId: "**************************************************************************************",
+    appId: "**************************************************************************************",
+    measurementId: "**************************************************************************************"
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
